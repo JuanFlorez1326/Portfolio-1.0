e160b0053e5036d7afe222b043470023d7119942
Juan Florez <jpatino@latinia.com>
2023-11-14 17:38:04 -0500

    CHANGE: URL Contact me

--- src/components/ContactMe/contact.jsx
+++ src/components/ContactMe/contact.jsx
@@ -2,14 +2,14 @@ import "./contact.css"
 import React from "react"
 import { AiOutlineMail } from "react-icons/ai"
 import { AiOutlineWhatsApp } from "react-icons/ai"
-import { AiOutlineInstagram } from "react-icons/ai"
+import { RiLinkedinFill } from "react-icons/ri";
 
 export const  Contact = () => {
   return (
     <div id="contact" className="container contact-container">
       <h1>Contact Me</h1>
       <div className="contact-links">
-        <a href="https://mail.google.com/mail/u/0/#inbox?compose=new" className="contact" target={"blank"}>
+        <a href="mailto:juanks339@gmail.com" className="contact" target={"blank"}>
           <AiOutlineMail className="icon" />
           <h2>Email <span>Juanks339@gmail.com</span></h2>
         </a>
@@ -19,9 +19,9 @@ export const  Contact = () => {
           <h2>WhatsApp <span>+57 313 706 6248</span></h2>
         </a>
 
-        <a href="https://www.instagram.com/juanflorez13/" className="contact instagram" target={"blank"}>
-          <AiOutlineInstagram className="icon" />
-          <h2>Instagram <span>JuanFlorez1326</span></h2>
+        <a href="https://www.linkedin.com/in/juanflorez1326" className="contact instagram" target={"blank"}>
+          <RiLinkedinFill className="icon" />
+          <h2>LinkedIn <span>JuanFlorez1326</span></h2>
         </a>
       </div>
     </div>
